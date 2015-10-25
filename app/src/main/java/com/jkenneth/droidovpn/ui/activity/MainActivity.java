b02d74d5931b300faad1c30767cb5d0f946fb2b3
jkennethcarino <=>
2015-10-25 19:22:18 +0800

    Update files for copyright line

--- app/src/main/java/com/jkenneth/droidovpn/ui/activity/MainActivity.java
+++ app/src/main/java/com/jkenneth/droidovpn/ui/activity/MainActivity.java
@@ -28,6 +28,26 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Shows the list of parsed servers from VPN Gate CSV
+ *
+ * Copyright (C) 2015  Jhon Kenneth Carino
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Created by Jhon Kenneth Carino on 10/18/15.
+ */
 public class MainActivity extends AppCompatActivity {
 
     private OkHttpClient mClient = new OkHttpClient();
